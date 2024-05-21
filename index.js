0a63548677560f65a4e0fb5ed4ab91f431f70f9f
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-21 15:55:26 -0700

    Add feature to resize grid

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const squares = document.querySelector(".squares");
 
-let numberOrSquares = 16 * 16;
+const body = document.querySelector("body");
 
-for (let i = 1; i <= numberOrSquares; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square")
-    squares.appendChild(square);
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    })
+const button = document.createElement("button");
+body.prepend(button);
+button.textContent = "Resize Grid";
+button.classList.add("button");
+
+let size = 16;
+
+button.addEventListener("click", () => {
+    size = prompt("Resize Grid (up to 100): ");
+    squares.innerHTML = "";
+    if (size < 1) size = 1;
+    if (size > 100) size = 100;
+    createGrid(size);
+});
+
+function createGrid(size) {
+    for (let i = 1; i <= size; i++) {
+        const row = document.createElement("div");
+        for (let j = 1; j <= size; j++) {
+            const column = document.createElement("div");
+            column.classList.add("square");
+            column.addEventListener("mouseenter", () => {
+                column.style.backgroundColor = "black";
+            });
+            row.appendChild(column);
+        }
+        squares.appendChild(row);
+    }
 }
+
+createGrid(size);
