678661ca598039ab7472ec4d54a7e60a0347684e
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-21 14:03:57 -0700

    Create 16x16 grid of square divs

---
+++ index.js
@@ -0,0 +1,9 @@
+const squares = document.querySelector(".squares");
+
+let numberOrSquares = 16 * 16;
+
+for (let i = 1; i <= numberOrSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square")
+    squares.appendChild(square);
+}
